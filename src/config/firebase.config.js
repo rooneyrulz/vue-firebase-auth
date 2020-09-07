fbf507062d49f75d581ce63e70d0b1c610b13c60
ruzny <ruzny@ruzny.com>
2020-09-07 11:49:39 +0530

    setup firebase config

---
+++ src/config/firebase.config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAu73XIJb49blNrjRUigOfUxkltKBZ_ShQ',
+    authDomain: 'vue-firebase-auth-2c60f.firebaseapp.com',
+    databaseURL: 'https://vue-firebase-auth-2c60f.firebaseio.com',
+    projectId: 'vue-firebase-auth-2c60f',
+    storageBucket: 'vue-firebase-auth-2c60f.appspot.com',
+    messagingSenderId: '123892679804',
+    appId: '1:123892679804:web:9fb0f671fc9cb45f0375f9',
+};
+
+firebase.initializeApp(firebaseConfig);
